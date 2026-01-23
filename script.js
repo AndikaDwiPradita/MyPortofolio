89346a562d538b53a0a6d01d8696c5550f04cc40
Andika Dwi Pradita <andikadwipradita8@gmail.com>
2026-01-23 15:35:39 +0700

    Add image Of Services And Add Animation

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+AOS.init({
+    duration: 1000, // Durasi animasi
+    offset: 100,    // Offset (dalam px) dari trigger point
+    delay: 100,     // Delay default untuk semua animasi
+    easing: 'ease', // Jenis easing
+    once: true,     // Animasi hanya sekali
+    mirror: false   // Tidak animasi lagi saat scroll balik
+});
+
 const navbarToggle=document.querySelector('.navbar-toggle');
 const navbarMenu=document.querySelector('.navbar-menu');
 
